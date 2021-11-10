965b674b593d1ff6c6632b2520ab144ca7624a9c
Stephen Chen <stephen.chen@cartesi.io>
2021-11-10 20:47:13 +0800

    Add manager test for signer middleware

--- tests/manager_test.rs
+++ tests/manager_test.rs
@@ -14,6 +14,7 @@ use common::*;
 
 use ethers::{
     providers::{Http, Middleware, Provider, Ws},
+    signers::{LocalWallet, Signer},
     types::{Address, U256},
     utils::GethInstance,
 };
@@ -21,8 +22,8 @@ use ethers::{
 use std::sync::Arc;
 use tokio::time::sleep;
 
-type TestTxManager = TransactionManager<
-    Factory<middleware_factory::HttpProviderFactory>,
+type TestTxManager<PF> = TransactionManager<
+    PF,
     BlockSubscriber<middleware_factory::WsProviderFactory>,
     usize,
 >;
@@ -41,6 +42,18 @@ async fn test_all() {
     increment_transaction(&tx_manager, &contract, l, account).await;
     wait(&tx_manager, l, 16, true).await;
     assert_increment(1, &contract).await;
+
+    let (tx_manager_with_signer, _subscriber_handle, signer) =
+        new_manager_with_signer(
+            &geth,
+            "380eb0f3d505f087e438eca80bc4df9a7faa24f868e69fc0440261a0fc0567dc",
+        )
+        .await;
+
+    // Send transaction via non-exist signer.
+    let l = new_label(&mut label_count);
+    increment_transaction(&tx_manager_with_signer, &contract, l, signer).await;
+    wait_err(&tx_manager_with_signer, l, 16).await;
 }
 
 ///
@@ -52,12 +65,14 @@ fn new_label(label: &mut usize) -> usize {
     ret
 }
 
-async fn wait(
-    tx_manager: &TestTxManager,
+async fn wait<PF>(
+    tx_manager: &TestTxManager<PF>,
     label: usize,
     iterations: usize,
     assert_final: bool,
-) {
+) where
+    PF: tx_manager::types::ProviderFactory + Send + Sync + 'static,
+{
     let mut i = 0;
     loop {
         if i >= iterations {
@@ -81,12 +96,37 @@ async fn wait(
     }
 }
 
-async fn increment_transaction(
-    tx_manager: &TestTxManager,
+async fn wait_err<PF>(
+    tx_manager: &TestTxManager<PF>,
+    label: usize,
+    iterations: usize,
+) where
+    PF: tx_manager::types::ProviderFactory + Send + Sync + 'static,
+{
+    let mut i = 0;
+    loop {
+        if i >= iterations {
+            panic!("transaction should fail with error");
+        }
+
+        let state_result = tx_manager.transaction_state(&label).await;
+        if state_result.is_err() {
+            return;
+        } else {
+            i += 1;
+            sleep(std::time::Duration::from_millis(500)).await;
+        }
+    }
+}
+
+async fn increment_transaction<PF>(
+    tx_manager: &TestTxManager<PF>,
     contract: &TestContract<Provider<Http>>,
     label: usize,
     from: Address,
-) {
+) where
+    PF: tx_manager::types::ProviderFactory + Send + Sync + 'static,
+{
     let strategy = ResubmitStrategy {
         gas_multiplier: None,
         gas_price_multiplier: None,
@@ -140,6 +180,62 @@ async fn new_manager(
         TransactionManager::new(factory, block_subscriber, retry, delay);
 
     (tx_manager, subscriber_handle)
+}
+
+async fn new_manager_with_signer(
+    geth: &GethInstance,
+    key: &str,
+) -> (
+    TransactionManager<
+        Factory<
+            middleware_factory::LocalSignerFactory<
+                middleware_factory::HttpProviderFactory,
+            >,
+        >,
+        BlockSubscriber<middleware_factory::WsProviderFactory>,
+        usize,
+    >,
+    BlockSubscriberHandle<Arc<Provider<Ws>>>,
+    Address,
+) {
+    let retry = 0;
+    let delay = std::time::Duration::from_millis(100);
+    let call_timeout = std::time::Duration::from_secs(1);
+    let block_period = std::time::Duration::from_secs(3);
+
+    let http_factory =
+        middleware_factory::HttpProviderFactory::new(geth.endpoint()).unwrap();
+
+    let ws_factory = middleware_factory::WsProviderFactory::new(
+        geth.ws_endpoint(),
+        retry,
+        delay,
+    )
+    .await
+    .unwrap();
+
+    let wallet: LocalWallet = key.parse().unwrap();
+    let signer_factory = middleware_factory::LocalSignerFactory::new(
+        http_factory,
+        wallet.clone(),
+    )
+    .await
+    .unwrap();
+
+    let (block_subscriber, subscriber_handle) =
+        BlockSubscriber::create_and_start(
+            ws_factory,
+            block_period,
+            retry,
+            delay,
+        );
+
+    let factory = Factory::new(signer_factory, call_timeout);
+
+    let tx_manager: TransactionManager<_, _, usize> =
+        TransactionManager::new(factory, block_subscriber, retry, delay);
+
+    (tx_manager, subscriber_handle, wallet.address())
 }
 
 async fn assert_increment(
